d077aa2dee14c3e3aae04cf52a3e58584656ad6f
BiradarSiddhant02 <pes1202102033@pesu.pes.edu>
2024-05-23 18:44:37 +0530

    error handling in ann.c

--- src/ann.c
+++ src/ann.c
@@ -10,24 +10,45 @@ Layer layer_builder(int input_dim, int output_dim, unsigned input_layer)
     Layer layer;
     layer.input_dim = input_dim;
     layer.output_dim = output_dim;
+    layer.bias = 0.0; // Default bias value
 
-    if (input_layer)
-        layer.bias = ((double)rand() / RAND_MAX); // Random bias between 0 and 1
-    else
-        layer.bias = 0.0; // No bias for non-input layers
-
+    // Allocate memory for weights
     layer.weights = (double**)malloc(layer.output_dim * sizeof(double*));
+    if (layer.weights == NULL) 
+    {
+        fprintf(stderr, "Memory allocation failed for weights in layer_builder\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Allocate memory for each weight array
     for (int i = 0; i < layer.output_dim; i++) 
     {
         layer.weights[i] = (double*)malloc(layer.input_dim * sizeof(double));
-        for (int j = 0; j < layer.input_dim; j++) 
-        {
-            layer.weights[i][j] = ((double)rand() / RAND_MAX); // Random weight between -1 and 1
+        if (layer.weights[i] == NULL) {
+
+            for (int j = 0; j < i; j++)
+                free(layer.weights[j]);
+
+            free(layer.weights);
+            fprintf(stderr, "Memory allocation failed for weights[%d] in layer_builder\n", i);
+            exit(EXIT_FAILURE);
         }
+    }
+
+    // If it's an input layer, set random bias
+    if (input_layer)
+        layer.bias = ((double)rand() / RAND_MAX); // Random bias between 0 and 1
+
+    // Initialize weights with random values
+    for (int i = 0; i < layer.output_dim; i++) 
+    {
+        for (int j = 0; j < layer.input_dim; j++) 
+            layer.weights[i][j] = ((double)rand() / RAND_MAX); // Random weight between -1 and 1
     }
 
     return layer;
 }
+
 
 Net network_builder(int num_layers, int input_dim, int* hidden_dims, int classes)
 {
@@ -36,32 +57,39 @@ Net network_builder(int num_layers, int input_dim, int* hidden_dims, int classes
     Net net;
     net.num_layers = num_layers;
     net.layers = (Layer*)malloc(num_layers * sizeof(Layer));
-    if (net.layers == NULL) {
+    if (net.layers == NULL)
+    {
         // Error handling
-        fprintf(stderr, "Memory allocation failed\n");
+        fprintf(stderr, "Memory allocation failed for net.layers in network_builder\n");
         exit(EXIT_FAILURE);
     }
 
     for(int layer = 0; layer < num_layers; layer++)
     {
         if(layer == 0)
-        {
-            // input layer, no bias
             net.layers[layer] = layer_builder(input_dim, hidden_dims[layer], YES);
-        }
+        
         else if(layer == num_layers - 1)
-        {
-            // output layer, output_dim equal to number of classes
             net.layers[layer] = layer_builder(net.layers[layer - 1].output_dim, classes, YES);
-        }
+        
         else
-        {
             net.layers[layer] = layer_builder(net.layers[layer - 1].output_dim, hidden_dims[layer], YES);
+        
+        if (net.layers[layer].weights == NULL) 
+        {
+
+            for (int i = 0; i < layer; i++) 
+                free(net.layers[i].weights);
+            
+            free(net.layers);
+            fprintf(stderr, "Error: layer_builder failed in network_builder for layer %d\n", layer);
+            exit(EXIT_FAILURE);
         }
     }
 
     return net;
 }
+
 
 void print_model_summary(Net net)
 {
